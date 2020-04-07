1ca93dbf1b727d97071626a9e2c2c0f5f331b122
Michel Bystranowski <bystrano@gmx.ch>
2020-04-07 10:25:37 +0200

    meilleure auto-complétion
    On cesse de proposer les arguments de commande une fois qu'on en a déjà saisi suffisamment.

--- spipsh_completion.sh
+++ spipsh_completion.sh
@@ -12,9 +12,22 @@
 #                                                                              #
 ################################################################################
 
+in_array() {
+    search="$1";
+    shift;
+
+    for item in "$@"; do
+        if [[ "$item" == "$search" ]]; then
+            return 0;
+        fi
+    done
+
+    return 1
+}
+
 _spipsh_completion()
 {
-    local cur opts cmds_array cmds cmd
+    local cur opts cmds_array cmds cmd cmd_args nb_expected_cmd_args
     COMPREPLY=()
     cur="${COMP_WORDS[COMP_CWORD]}"
     opts="-a -d -c -t -l --all --dry-run --connect --tmp-dir --local-dir"
@@ -36,21 +49,41 @@ _spipsh_completion()
     )
     cmds="${cmds_array[@]}"
 
+    # Parser les options déjà saisies.
+    ignore_next=0
+    cmd_args=()
     for word in "${COMP_WORDS[@]}"; do
-        if [[ -n "$word" ]] && [[ ! "$word" =~ ^- ]]; then
-            for command in "${cmds_array[@]}"; do
-                if [[ "$command" == "$word" ]]; then
-                    cmd="$word";
-                    break 2;
-                fi
-            done
+        if [[ -z "$word" ]]; then
+            continue;
+        elif [[ "$word" =~ ^- ]]; then
+            # echo "opt: $word"
+            if in_array "$word" '-t' '-d' '-c' '--connect' '--tmp-dir' '--local-dir'; then
+                ignore_next=1
+            fi
+        elif [[ $ignore_next -gt 0 ]]; then
+            # echo "optarg: $word"
+            ((ignore_next--))
+        elif [[ -z "${cmd+x}" ]]; then
+            if in_array "$word" "${cmds_array[@]}"; then
+                # echo "cmd: $word"
+                cmd="$word"
+            fi
+        elif [[ -n "${cmd+x}" ]]; then
+            # echo "cmd_arg: $word"
+            cmd_args+=("$word")
         fi
     done
+
+    if [[ ${#cur} -eq 0 ]]; then
+        nb_expected_cmd_args=0
+    else
+        nb_expected_cmd_args=1
+    fi
 
     if [[ -z "${cmd:+x}" ]] && [[ ! "${cur}" =~ ^- ]]; then
         COMPREPLY=( $(compgen -W "${cmds}" -- ${cur}) )
         return 0
-    elif [[ -n "${cmd:+x}" ]] && [[ ! "${cur}" =~ ^- ]]; then
+    elif [[ -n "${cmd:+x}" ]] && [[ ! "${cur}" =~ ^- ]] && [[ ${#cmd_args[@]} -eq $nb_expected_cmd_args ]]; then
         case "${cmd}" in
             db-import)
                 local running=$(find "."\
@@ -76,7 +109,7 @@ _spipsh_completion()
                 return 0
                 ;;
         esac
-    elif [[ "${cur}" =~ ^- ]]; then
+    else
         COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
         return 0
     fi
