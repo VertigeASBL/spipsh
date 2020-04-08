93eac9a2091fbef996a2664f51289cfb96a12dae
Michel Bystranowski <bystrano@gmx.ch>
2020-04-08 11:46:34 +0200

    nettoyage

--- cmd/init.sh
+++ cmd/init.sh
@@ -9,11 +9,18 @@ if [[ ! -f "$file" ]] || [[ ! "$file" =~ /spipsh$ ]]; then
     exit 2;
 fi
 
-for dir in $SPIP_NEEDED_DIRS; do
+# Les dossiers qui doivent exister pour que SPIP fonctionne sans embrouilles
+needed_dirs="config IMG local lib tmp plugins plugins/auto plugins/fabrique_auto squelettes"
+
+# Les dossiers qui doivent être accessibles en écriture par Apache
+writeable_dirs="config IMG local lib tmp plugins/auto plugins/fabrique_auto"
+
+for dir in $needed_dirs; do
     if [[ ! -d $dir ]]; then
         do_and_tell "crée le dossier $dir"\
                     mkdir "$dir";
     fi
 done
+
 do_and_tell "répare les permissions"\
-            chmod -R a+rwX $SPIP_WRITEABLE_DIRS;
+            chmod -R a+rwX "$writeable_dirs";
