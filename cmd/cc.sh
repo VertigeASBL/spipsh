d5dd94d3c30fa90f9427a7c71359c018674b99f7
Michel Bystranowski <bystrano@gmx.ch>
2020-04-16 09:00:43 +0200

    utiliser les évolutions du framework
    On peut maintenant avoir des options spécifiques à des sous-commandes. On passe
    alors les options spécifiques à cc.sh dans cc.sh.

--- cmd/cc.sh
+++ cmd/cc.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
 # Summary : Vide les caches du dossier tmp, à la dure, sans passer par SPIP.
+#
 # Env : spip
+#
+# Options :
+#
+# % all
+# desc="Permet de vider aussi le cache des vignettes."
+# short="a" type="flag" variable="all" value="1" default=0
+#
+# % tmp-dir
+# desc="Permet de choisir un répertoire tmp/ alternatif."
+# short="t" type="option" variable="tmp_dir" default="tmp"
+#
+# % local-dir
+# desc="Permet de choisir un répertoire local/ alternatif."
+# short="l" type="option" variable="local_dir" default="local"
+#
 set -euo pipefail
+
+
+# shellcheck disable=2154
+if [[ ! -d "$tmp_dir" ]]; then
+    out_usage_error "$tmp_dir n'est pas un dossier."
+fi
+
+# shellcheck disable=2154
+if [[ ! -d "$local_dir" ]]; then
+    out_usage_error "$local_dir n'est pas un dossier."
+fi
+
 
 # shellcheck disable=2154
 out_exec "vide le cache"\
